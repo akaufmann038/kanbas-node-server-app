a1ae93160902527891efb177d422e2ce9459a007
Alexander Kaufmann <alexkaufmann@Alexanders-MacBook-Pro.local>
2023-12-04 11:14:09 -0500

    homework 6

--- app.js
+++ app.js
@@ -4,17 +4,30 @@ import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
 import "dotenv/config";
 
 const app = express();
 app.use(
   cors({
-    credential: true,
+    credentials: true,
     origin: process.env.FRONTEND_URL,
   })
 );
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(session(sessionOptions));
+
 app.use(express.json());
 
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Hello(app);
