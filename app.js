b88ef3c1be0639734e94d316310b4721e2294785
Alexander Kaufmann <alexkaufmann@Alexanders-MacBook-Pro.local>
2023-12-04 12:36:31 -0500

    added connection to mongo atlas

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ import cors from "cors";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 import session from "express-session";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
-
 import "dotenv/config";
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(
@@ -23,6 +24,13 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
 app.use(session(sessionOptions));
 
 app.use(express.json());
